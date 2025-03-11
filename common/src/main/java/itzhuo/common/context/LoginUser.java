fd7f99674aad20226c06262bae6f3288ceb6a427
zhuochengtong <2546563316@qq.com>
2025-03-11 17:10:02 +0800

    修改获取不到当前用户信息bug

--- common/src/main/java/itzhuo/common/context/LoginUser.java
+++ common/src/main/java/itzhuo/common/context/LoginUser.java
@@ -1,10 +1,17 @@
 package itzhuo.common.context;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+/**
+ * 表示当前登录用户的信息。
+ * 包含用户的唯一标识符（userId）和用户名（username）。
+ */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginUser {
-
-    private String id;
+    private String userId;
     private String username;
 }
